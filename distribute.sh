03e92498ff6e58bf5568108822a9260bafc473a3
Gagik Gavalian <gavalian@meson.jlab.org>
2015-03-18 15:59:52 -0400

    added a script to distribute the documentation

---
+++ distribute.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+scp -r Detectors/build/html jlabl1:/group/clas/www/clasweb/html/clas12offline/docs/detectors/.
+scp -r OfflineSoftware/build/html jlabl1:/group/clas/www/clasweb/html/clas12offline/docs/software/.
